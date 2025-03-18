6e1670d3a43c78a70acbcd38570dbf3e58f868b2
Midlaj <muhammedmidlaj8395@gmail.com>
2025-03-18 13:58:57 +0530

    again added

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../Models/userModels.js';
 import dotenv from 'dotenv';
@@ -8,7 +8,7 @@ dotenv.config();
 export const signup = async (req, res) => {
   const { name, email, password } = req.body;
   try {
-    const hashedPassword = bcrypt.hashSync(password, 10);
+    const hashedPassword = bcrypt.hash(password, 10);
     const user = await User.create({ name, email, password: hashedPassword });
     res.status(201).json({ message: 'User registered successfully', user });
   } catch (err) {
@@ -43,7 +43,7 @@ export const login = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ where: { email } });
-    if (!user || !bcrypt.compareSync(password, user.password)) {
+    if (!user || !bcrypt.compare(password, user.password)) {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
     const token = jwt.sign({ id: user.id },process.env.JWT_SECRET,{ expiresIn:'7h' });
